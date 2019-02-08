41b430d1f94a2224e0b74b8c3ffe12ca84774ddd
Davide Bettio <davide.bettio@ispirata.com>
2019-02-08 11:15:37 +0100

    Start again from 0

--- config/config.exs
+++
@@ -1,4 +0,0 @@
-use Mix.Config
-
-config :bson,
-    decoder_new_doc: &Bson.Decoder.elist_to_atom_map/1
--- test/test_helper.exs
+++
@@ -1,8 +0,0 @@
-ExUnit.start
-
-defmodule TestAll do
-	def now(yes_no) do
-		yes_no || false
-	end
-	
-end
